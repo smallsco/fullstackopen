4d38341710038f3dc2a14cafcb45da29cb920454
Scott Small <smallsco@gmail.com>
2020-04-07 17:00:53 -0700

    1.7 unicafe step2

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,6 +7,10 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const total = good + neutral + bad
+  const average = (good + (-1*bad))/total
+  const positive_percentage = (good / total) * 100
+
   return (
     <div>
       <h1>Give Feedback</h1>
@@ -16,7 +20,10 @@ const App = () => {
       <h1>Statistics</h1>
       Good: {good}<br />
       Neutral: {neutral}<br />
-      Bad: {bad}
+      Bad: {bad}<br />
+      Total: {total}<br />
+      Average: {average}<br />
+      Positive: {positive_percentage}%
     </div>
   )
 }
