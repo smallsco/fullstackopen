33cf1895a9c14ec9244903d841b862adb803961f
Scott Small <smallsco@gmail.com>
2020-04-08 16:07:50 -0700

    2.1 / 2.2 / 2.3 course information steps 6/7/8

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ text }) => (
+  <h1>{text}</h1>
+)
+
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    { parts.map( part =>
+      <Part key={part.id} name={part.name} exercises={part.exercises} />
+    ) }
+  </div>
+)
+
+const Total = ({ parts }) => (
+  <p>
+    <b>
+      Total of { parts.reduce( (sum, part) => (
+        sum + part.exercises
+      ), 0) } exercises
+    </b>
+  </p>
+)
+
+const Course = ({ course }) => (
+  <div>
+    <Header text={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
