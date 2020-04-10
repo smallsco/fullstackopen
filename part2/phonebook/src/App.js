649f11a6b4547581db149b2e06d48936b9301f84
Scott Small <smallsco@gmail.com>
2020-04-10 14:19:06 -0700

    2.15 phonebook step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -24,7 +24,10 @@ const App = () => {
     }
     else
     {
-      setPersons(persons.concat({name: newName, number: newNumber}))
+      const newPerson = {name: newName, number: newNumber}
+      axios.post('http://localhost:3001/persons', newPerson).then(response => {
+        setPersons(persons.concat(response.data))
+      })
     }
     setNewName('')
     setNewNumber('')
