054f5543ffcd781590ed9dada14fffb05001d296
Scott Small <smallsco@gmail.com>
2020-04-15 16:27:16 -0700

    4.4 helper functions and unit tests step2

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,10 @@
-const dummy = (blogs) => 1
+const dummy = blogs => 1
+
+const totalLikes = blogs => (
+  blogs.reduce((sum, blog) => sum + blog.likes, 0)
+)
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
