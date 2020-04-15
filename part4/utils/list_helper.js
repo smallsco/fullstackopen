d46b21e8696a76e4d03944f16b5035dc68cbf988
Scott Small <smallsco@gmail.com>
2020-04-15 16:50:35 -0700

    4.5 helper functions and unit tests step3

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,10 +1,31 @@
+// Always returns 1.
 const dummy = blogs => 1
 
+// Given a list of blogs, return the total likes.
 const totalLikes = blogs => (
   blogs.reduce((sum, blog) => sum + blog.likes, 0)
 )
 
+// Given a list of blogs, return the blog with the most likes.
+const favouriteBlog = blogs => {
+  let max = 0
+  let favourite = null
+  blogs.map((blog) => {
+    if (blog.likes > max) {
+      max = blog.likes
+      favourite = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    }
+  })
+  return favourite
+}
+
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
