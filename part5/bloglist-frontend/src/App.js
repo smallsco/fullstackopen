ef7fb61212d479d98c909bbe3c23af6b9a10f02f
Scott Small <smallsco@gmail.com>
2020-04-21 16:56:00 -0700

    5.2 bloglist frontend step2

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -13,12 +13,24 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [loggedInUser, setLoggedInUser] = useState(null)
 
+  // Get all blogs when component renders.
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
   }, [])
 
+  // Check local storage when component renders to see if a user is currently
+  // logged in.
+  useEffect(() => {
+    const userJSON = window.localStorage.getItem('fsoblogapp.loggedinuser')
+    if (userJSON) {
+      const user = JSON.parse(userJSON)
+      setLoggedInUser(user)
+    }
+  }, [])
+
+  // Update username/password state when user types into the login form.
   const onChangeUsername = (event) => {
     setUsername(event.target.value)
   }
@@ -26,19 +38,37 @@ const App = () => {
     setPassword(event.target.value)
   }
 
+  // Logs in a user
   const onLogin = async (event) => {
     event.preventDefault()
     try {
       const user = await loginService.login(username, password)
-      setLoggedInUser(user)
-      setUsername('')
-      setPassword('')
+
+      if (user.hasOwnProperty('error')) {
+        console.log(user.error)
+      }
+      else {
+        setUsername('')
+        setPassword('')
+        setLoggedInUser(user)
+        window.localStorage.setItem(
+          'fsoblogapp.loggedinuser',
+          JSON.stringify(user)
+        )
+      }
     }
     catch (error) {
       console.log(error)
     }
   }
 
+  // Logs out a user
+  const onLogout = () => {
+    window.localStorage.removeItem('fsoblogapp.loggedinuser')
+    setLoggedInUser(null)
+  }
+
+  // Render blogs or login form
   return (
     <>
       {!loggedInUser &&
@@ -52,7 +82,10 @@ const App = () => {
       }
       {loggedInUser &&
         <div>
-          <p>Welcome, {loggedInUser.name}!</p>
+          <p>
+            Welcome, {loggedInUser.name}!
+            <button onClick={onLogout}>Logout</button>
+          </p>
           <h2>Blogs</h2>
           {blogs.map(blog =>
             <Blog key={blog.id} blog={blog} />
--- part5/bloglist-frontend/src/services/login.js
+++ part5/bloglist-frontend/src/services/login.js
@@ -1,8 +1,23 @@
 import axios from 'axios'
 
 const login = async (username, password) => {
-  const response = await axios.post('/api/login', {username, password})
-  return response.data
+  try {
+    const response = await axios.post('/api/login', {username, password})
+    return response.data
+  }
+  catch (error) {
+    if (error.response.data.error) {
+      // backend returns errors as json
+      return error.response.data
+    }
+    else if (error.response.data) {
+      // handle proxy errors which are returned by strings
+      return {error: error.response.data}
+    }
+    else {
+      return {error: "Unknown error occurred during login. Check backend logs."}
+    }
+  }
 }
 
 export default { login }
