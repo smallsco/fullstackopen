8d5900f2b8c2a0c9cbbf1ca1c06036eb75a7f225
Scott Small <smallsco@gmail.com>
2020-04-27 11:50:44 -0700

    6.2 unicafe revisited step2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,21 +6,41 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const onGood = () => {
     store.dispatch({
       type: 'GOOD'
     })
   }
 
+  const onNeutral = () => {
+    store.dispatch({
+      type: 'OK'
+    })
+  }
+
+  const onBad = () => {
+    store.dispatch({
+      type: 'BAD'
+    })
+  }
+
+  const onReset = () => {
+    store.dispatch({
+      type: 'ZERO'
+    })
+  }
+
+  const state = store.getState()
+
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>neutral</button> 
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>neutral</div>
-      <div>bad</div>
+      <button onClick={onGood}>good</button>
+      <button onClick={onNeutral}>neutral</button>
+      <button onClick={onBad}>bad</button>
+      <button onClick={onReset}>reset stats</button>
+      <div>Good: {state.good}</div>
+      <div>Neutral: {state.ok}</div>
+      <div>Bad: {state.bad}</div>
     </div>
   )
 }
