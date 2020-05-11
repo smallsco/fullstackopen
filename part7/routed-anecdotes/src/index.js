eed5ff4cbc04c29aa6dce9130c4e5951b63e3692
Scott Small <smallsco@gmail.com>
2020-05-11 10:54:40 -0700

    7.1 routed anecdotes step1

---
+++ part7/routed-anecdotes/src/index.js
@@ -0,0 +1,10 @@
+// Third-Party Imports
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+// My Imports
+import App from './App'
+
+// Render app
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'))
