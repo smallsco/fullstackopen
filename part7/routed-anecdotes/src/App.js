2cb858bcc32dc4d83f031309e2dd455d2a171abb
Scott Small <smallsco@gmail.com>
2020-05-11 11:42:27 -0700

    7.4 anecdotes and hooks step1

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -8,6 +8,9 @@ import {
   useRouteMatch
 } from "react-router-dom"
 
+// My Imports
+import { useField } from './hooks'
+
 
 // Site Navigation Menu
 const Menu = () => {
@@ -68,22 +71,22 @@ const Footer = () => (
 
 // Create new anecdote view
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const history = useHistory()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     history.push('/')
-    props.setNotification(`New Anecdote "${content}" created!`)
+    props.setNotification(`New Anecdote "${content.value}" created!`)
     setTimeout(() => props.setNotification(''), 10000)
   }
 
@@ -93,15 +96,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input {...content} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
