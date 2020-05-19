886962fb15a1b5e53c861a2c82b7e353e33b40d4
Scott Small <smallsco@gmail.com>
2020-05-19 16:35:17 -0700

    8.10 adding a book

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,33 +1,54 @@
+// Third-Party Dependencies
 import React, { useState } from 'react'
+import { useMutation } from '@apollo/client'
+
+// My Dependencies
+import { ALL_AUTHORS, ALL_BOOKS, CREATE_BOOK } from '../queries'
 
 const NewBook = (props) => {
   const [title, setTitle] = useState('')
-  const [author, setAuhtor] = useState('')
+  const [author, setAuthor] = useState('')
   const [published, setPublished] = useState('')
   const [genre, setGenre] = useState('')
   const [genres, setGenres] = useState([])
 
+  // When adding a new book keep the all books/authors views up to date
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [ { query: ALL_BOOKS }, { query: ALL_AUTHORS } ]
+  })
+
+  // Do not show this page if we are showing another page
   if (!props.show) {
     return null
   }
 
+  // Adds a new book to the server using a GraphQL mutation
   const submit = async (event) => {
     event.preventDefault()
-    
-    console.log('add book...')
+
+    createBook({
+      variables: {
+        title,
+        author,
+        published: Number(published),
+        genres
+      }
+    })
 
     setTitle('')
     setPublished('')
-    setAuhtor('')
+    setAuthor('')
     setGenres([])
     setGenre('')
   }
 
+  // Adds additional genres to a book
   const addGenre = () => {
     setGenres(genres.concat(genre))
     setGenre('')
   }
 
+  // Renders the add new book form
   return (
     <div>
       <form onSubmit={submit}>
@@ -42,7 +63,7 @@ const NewBook = (props) => {
           author
           <input
             value={author}
-            onChange={({ target }) => setAuhtor(target.value)}
+            onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
         <div>
