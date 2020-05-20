3bd3773bb67e89742bd6e9ee44c6f45f873367e2
Scott Small <smallsco@gmail.com>
2020-05-20 12:51:35 -0700

    8.13 database part1

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,85 +1,18 @@
-const { ApolloServer, gql } = require('apollo-server')
-const _ = require('lodash')
-const { v1: uuid } = require('uuid')
+// Third-Party Imports
+require('dotenv').config()
+const { ApolloServer, UserInputError, gql } = require('apollo-server')
+const mongoose = require('mongoose')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: "afa51ab0-344d-11e9-a414-719c6709cf3e",
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: "afa5b6f0-344d-11e9-a414-719c6709cf3e",
-    born: 1963
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: "afa5b6f1-344d-11e9-a414-719c6709cf3e",
-    born: 1821
-  },
-  { 
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: "afa5b6f2-344d-11e9-a414-719c6709cf3e",
-  },
-  { 
-    name: 'Sandi Metz', // birthyear not known
-    id: "afa5b6f3-344d-11e9-a414-719c6709cf3e",
-  },
-]
+// My Imports
+const Author = require('./models/author')
+const Book = require('./models/book')
 
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: "afa5b6f5-344d-11e9-a414-719c6709cf3e",
-    genres: ['agile', 'patterns', 'design']
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: "afa5de00-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring']
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: "afa5de01-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'patterns']
-  },  
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: "afa5de02-344d-11e9-a414-719c6709cf3e",
-    genres: ['refactoring', 'design']
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de03-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'crime']
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: "afa5de04-344d-11e9-a414-719c6709cf3e",
-    genres: ['classic', 'revolution']
-  },
-]
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true)
+mongoose.set('useFindAndModify', false)
 
+// GraphQL Schema
 const typeDefs = gql`
   type Author {
     name: String!
@@ -91,9 +24,9 @@ const typeDefs = gql`
   type Book {
     title: String!
     published: Int!
-    author: String!
-    id: ID!
+    author: Author!
     genres: [String!]!
+    id: ID!
   }
 
   type Query {
@@ -117,59 +50,66 @@ const typeDefs = gql`
   }
 `
 
+// GraphQL Resolvers
 const resolvers = {
   Query: {
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      let booksToReturn = books
-      if (args.author) {
-        booksToReturn = booksToReturn.filter(book => book.author === args.author)
-      }
-      if (args.genre) {
-        booksToReturn = booksToReturn.filter(book => book.genres.includes(args.genre))
-      }
-      return booksToReturn
+      return Book.find({})
+      // FIXME: need filtering for args.author / args.genre
     },
     allAuthors: () => {
-      const bookCounts = _.countBy(books, book => book.author)
-      return authors.map(author => ({...author, bookCount: bookCounts[author.name]}))
+      return Author.find({})
+      // FIXME: need to implement bookCount for authors
     },
-    bookCount: () => books.length
+    bookCount: () => Book.estimatedDocumentCount()
   },
   Mutation: {
-    addBook: (root, args) => {
-      const newBook = {...args, id: uuid()}
-      books = books.concat(newBook)
-
-      const authorExists = authors.find(author => author.name === args.author)
-      if (!authorExists) {
-        const newAuthor = {
+    addBook: async (root, args) => {
+      let author = await Author.findOne({ name: args.author })
+      if (!author) {
+        author = new Author({
           name: args.author,
-          born: null,
-          id: uuid()
+          born: null
+        })
+        try {
+          await author.save()
         }
-        authors = authors.concat(newAuthor)
+        catch (error) {
+          throw new UserInputError(error.message, {
+            invalidArgs: args,
+          })
+        }
       }
-
+      const newBook = new Book({...args, author})
+      try {
+        await newBook.save()
+      }
+      catch (error) {
+        throw new UserInputError(error.message, {
+          invalidArgs: args,
+        })
+      }
       return newBook
     },
     editAuthor: (root, args) => {
-      const authorToEdit = authors.find(author => author.name === args.name)
+      // FIXME: needs update to work with the database
+      /*const authorToEdit = authors.find(author => author.name === args.name)
       if (!authorToEdit) {
         return null
       }
       authorToEdit.born = args.setBornTo
       authors = authors.map(author => author.name === args.name ? authorToEdit : author)
-      return authorToEdit
+      return authorToEdit*/
     }
   }
 }
 
+// Run GraphQL Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
-
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
