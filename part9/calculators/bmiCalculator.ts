b558eabc6da8a13fbc511b102e61290e324a921b
Scott Small <smallsco@gmail.com>
2020-05-31 12:06:29 -0700

    9.5 WebBMI

--- part9/calculators/bmiCalculator.ts
+++ part9/calculators/bmiCalculator.ts
@@ -1,16 +1,4 @@
-const calculateBmi = (args: Array<string>): String => {
-
-  // Sanity Check Inputs
-  if (args.length != 4) {
-    throw new Error('Incorrect number of arguments');
-  }
-  const height: number = Number(args[2]);
-  const weight: number = Number(args[3]);
-  if (isNaN(height) || isNaN(weight)) {
-    throw new Error('Height and Weight must be numbers');
-  }
-
-  // Do the BMI Calculation
+export const calculateBmi = (height: number, weight: number): string => {
   const height_in_metres: number = height / 100;
   const bmi: number = weight / (Math.pow(height_in_metres, 2));
   if (bmi < 18.5) {
@@ -22,9 +10,25 @@ const calculateBmi = (args: Array<string>): String => {
   else if (bmi >= 25 && bmi <= 29.9) {
     return 'Overweight';
   }
-  else if (bmi >= 30) {
+  else /*if (bmi >= 30)*/ {
     return 'Obese';
   }
 }
 
-console.log(calculateBmi(process.argv));
+// Calculate BMI using command line args if the script was called directly
+if (require.main === module) {
+
+  // Sanity Check Input
+  const args: Array<string> = process.argv;
+  if (args.length != 4) {
+    throw new Error('Incorrect number of arguments');
+  }
+  const height: number = Number(args[2]);
+  const weight: number = Number(args[3]);
+  if (isNaN(height) || isNaN(weight)) {
+    throw new Error('Height and Weight must be numbers');
+  }
+
+  // Do the BMI Calculation
+  console.log(calculateBmi(height, weight));
+}
--- part9/calculators/index.ts
+++ part9/calculators/index.ts
@@ -1,5 +1,8 @@
 // Third-Party Imports
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
+
+// My Imports
+import { calculateBmi } from './bmiCalculator';
 
 // Init Express Framework
 const app = express();
@@ -8,6 +11,27 @@ const app = express();
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
+app.get('/bmi', (req, res) => {
+  // Sanity Check Input
+  if (!req.query.height || !req.query.weight) {
+    res.status(400).json({ error: 'Height and Weight must be provided' });
+  }
+  const height: number = Number(req.query.height);
+  const weight: number = Number(req.query.weight);
+  if (isNaN(height) || isNaN(weight)) {
+    res.status(400).json({ error: 'Height and Weight must be numbers' });
+  }
+
+  // Do the BMI Calculation
+  const bmi: string = calculateBmi(height, weight);
+  res.json({weight, height, bmi});
+});
+
+// Error Handler
+const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+  res.status(500).json({ error: error.message });
+}
+app.use(errorHandler);
 
 // Start Server
 const PORT = 3003;
