7dc25b567becea3ea3fb657e7947a5e3e2608235
Scott Small <smallsco@gmail.com>
2020-05-31 14:09:48 -0700

    9.9 patientor backend step2

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,16 +1,18 @@
 // Third-Party Imports
 import express from 'express';
+import cors from 'cors';
 
 // Init Express Framework
 const app = express();
+app.use(cors());  // eslint-disable-line @typescript-eslint/no-unsafe-call
 
 // Routes
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   return res.send('pong');
 });
 
 // Start Server
-const PORT = 3000;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
